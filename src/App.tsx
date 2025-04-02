48f7215a399287ca7bb65d5e69981021c60fa973
Mário César <mariocesarmoliveirafilho@gmail.com>
2025-04-02 17:17:01 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+// Importes de telas
+import './index.css'
+import HomeScreen from './pages/Home/HomeScreen'
+
+function App() {
+  return (
+    <>
+      <HomeScreen />
+    </>
+  )
+}
+
+export default App
